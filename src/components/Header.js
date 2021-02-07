e09b2d3735e04202b2dd4ff36092fb8d67703e2c
LeoXu1 <leo.xu.64@gmail.com>
2021-02-06 20:26:00 -0500

    Random number generation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,26 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { random: 1 };
+  }
+  handleClick() {
+    console.log('click')
+    this.state.random = Math.floor(Math.random()*5);
+    this.setState({ random: this.state.random });
+  }
+  render() {
     return (
         <header className='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Spin' onClick={onClick} />
+            <h1>For a Day</h1>
+            <h2>{this.state.random}</h2>
+            <Button color='green' text='Spin' onClick={this.handleClick.bind(this)} />
         </header>
     )
+  }
 }
-
-Header.defaultProps = {
-    title: 'For-a-Day',
-}
-
-Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-
 export default Header
