6487880f946ad944a6fa77056932588c29df195d
kmohan00 <52424363+kmohan00@users.noreply.github.com>
2021-02-06 23:08:47 -0500

    Update MainWheel.java

--- MainWheel.java
+++ MainWheel.java
@@ -19,38 +19,106 @@ public class MainWheel {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		ArrayList<String> endList = new ArrayList<String>();
-		endList.add("Recycle");
-		endList.add("Take the bus");
-		endList.add("Text a friend");
-		endList.add("Call your grandparents");
-		endList.add("Read a book");
-		endList.add("Take a walk");
-		endList.add("Ride a bike");
-		endList.add("Pick up trash");
-		endList.add("Eat healthy");
-		endList.add("Take a shorter shower");
-		endList.add("Go to bed early");
-		endList.add("Go vegan");
-		endList.add("Go vegetarian");
-		endList.add("Use reusable bags");
-		endList.add("Use reusable straws");
+		ArrayList<String> endList1 = new ArrayList<String>();
+		endList1.add("Recycle");
+		endList1.add("Take the bus");
+		endList1.add("Text a friend");
+		endList1.add("Call your grandparents");
+		endList1.add("Read a book");
+		endList1.add("Take a walk");
+		ArrayList<String> endList2 = new ArrayList<String>();
+		endList2.add("Use reusable straws");
+		endList2.add("Pick up trash");
+		endList2.add("Eat healthy");
+		endList2.add("Take a shorter shower");
+		endList2.add("Go to bed early");
+		endList2.add("Go vegan");
+		ArrayList<String> endList3 = new ArrayList<String>();
+		endList3.add("Go vegetarian");
+		endList3.add("Use reusable bags");
+		endList3.add("Grow a plant");
+		endList3.add("Meditate");
+		endList3.add("Volunteer");
+		endList3.add("Ride a bike");
+		ArrayList<String> endList4 = new ArrayList<String>();
+		endList4.add("Journal");
+		endList4.add("Discover a new hobby");
+		endList4.add("Make a craft out of trash");
+		endList4.add("Donate to a charity");
+		endList4.add("Cook a new meal");
+		endList4.add("Clean");
+		ArrayList<String> endList5 = new ArrayList<String>();
+		endList5.add("Encourage someone");
+		endList5.add("Unplug nonessential devices");
+		endList5.add("Dance");
+		endList5.add("Write a letter");
+		endList5.add("Give a gift");
+		endList5.add("Go plastic-free");
+		ArrayList<String> endList6 = new ArrayList<String>();
+		endList6.add("Sing");
+		endList6.add("Turn off your tv");
+		endList6.add("Meet someone new");
+		endList6.add("Call a relative");
+		endList6.add("Give a gift");
+		endList6.add("Draw");
 
 		ArrayList<String> list = new ArrayList<String>();
 		int count = 0;
-		while (count < 6) {
-			list.add(endList.get(random.nextInt(14)));
-			count++;
+
+		int listChooser = random.nextInt(6);
+		switch (listChooser) {
+		case 1:
+			while (count < 6) {
+				list.add(endList1.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		case 2:
+			while (count < 6) {
+				list.add(endList2.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		case 3:
+			while (count < 6) {
+				list.add(endList3.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		case 4:
+			while (count < 6) {
+				list.add(endList4.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		case 5:
+			while (count < 6) {
+				list.add(endList5.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		case 6:
+			while (count < 6) {
+				list.add(endList6.get(random.nextInt(5)));
+				count++;
+			}
+			break;
+		default:
+			while (count < 6) {
+				list.add(endList1.get(random.nextInt(5)));
+				count++;
+			}
+			break;
 		}
 
 		Wheel wheel = new Wheel(list);
 		wheel.hasBorders(true);
 		wheel.setBounds(100, 100, 400, 400);
 
-		JLabel lbl1 = new JLabel("Selection: ");
+		JLabel lbl1 = new JLabel("Activity: ");
 		JLabel lbl2 = new JLabel("Angle: ");
 		JLabel lbl3 = new JLabel("Speed: ");
-		JLabel lblsel = new JLabel("(selection)");
+		JLabel lblsel = new JLabel("(activity)");
 		JLabel lblang = new JLabel("(angle)");
 		JLabel lblsp = new JLabel("(speed)");
 		JButton btnSpin = new JButton("Spin");
@@ -61,7 +129,7 @@ public class MainWheel {
 		lblang.setBounds(830, 30, 150, 20);
 		lbl3.setBounds(720, 50, 100, 20);
 		lblsp.setBounds(830, 50, 150, 20);
-		btnSpin.setBounds(720, 70, 100, 20);
+		btnSpin.setBounds(520, 290, 100, 20);
 		frame.add(wheel);
 		frame.add(lbl1);
 		frame.add(lblsel);
@@ -83,12 +151,10 @@ public class MainWheel {
 		btnSpin.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				// do everything here...
 				try {
 					if (wheel.isSpinning() == false)
 						wheel.spinStartAsync(180, 3, -40);
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
