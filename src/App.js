47f3a73c6f64c6bd9e59927502ab19b23a97cdf3
LeoXu1 <leo.xu.64@gmail.com>
2021-02-07 00:41:06 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,71 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import basethumb from './smileys/basethumb.png'
 
   const App = () => {
     const [tasks, setTasks] = useState([
         {
             id: 1,
+            day: 'Sunday',
             text: 'Take a Walk',
+            points: 40,
         },
         {
             id: 2,
+            day: 'Monday',
             text: 'Take Public Transportation',
+            points: 50
         },
         {
             id: 3,
+            day: 'Tuesday',
             text: 'Pick Up Trash',
+            points: 30
         },
         {
             id: 4,
+            day: 'Wednesday',
             text: 'Eat Healthy',
+            points: 45
         },
         {
             id: 5,
-            text: 'Talk to a Neighbor'
+            day: 'Thursday',
+            text: 'Recycle',
+            points: 30
+        },
+        {
+            id: 6,
+            day: 'Friday',
+            text: 'Meditate',
+            points: 20
+        },
+        {
+          id: 7,
+          day: 'Saturday',
+          text: 'Volunteer',
+          points: 100
+        },
+        {
+          id: 8,
+          day: "Week Total",
+          points: 315
         }
     ])
-}
+
+ // Delete Task
+ const deleteTask = (id) => {
+   console.log('delete', id)
+ }
 
   return (
     <div className='container'>
       <Header />
+      <p> Do something new to help the environment each day.</p>
       <Tasks tasks={tasks}/>
+      <img src={basethumb} width = '600px'/>
     </div>
   )
-
+  }
 export default App;
