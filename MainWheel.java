cdd9675ddeb5fdd365be3dac914a088c766cc7d1
kmohan00 <52424363+kmohan00@users.noreply.github.com>
2021-02-07 00:41:15 -0500

    Update MainWheel.java

--- MainWheel.java
+++ MainWheel.java
@@ -1,3 +1,4 @@
+import java.applet.Applet;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class MainWheel {
+public class MainWheel extends Applet {
 
 	public static void main(String[] args) throws Exception {
 
